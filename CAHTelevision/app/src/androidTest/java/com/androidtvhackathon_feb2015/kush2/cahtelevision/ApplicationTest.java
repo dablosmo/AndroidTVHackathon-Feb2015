951e04e3c1a25842c72c11dbef4dbde871d484ef
kushaank <kushaank@usc.edu>
2015-02-07 11:43:36 -0800

    Initialized TVapp

---
+++ CAHTelevision/app/src/androidTest/java/com/androidtvhackathon_feb2015/kush2/cahtelevision/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.androidtvhackathon_feb2015.kush2.cahtelevision;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
