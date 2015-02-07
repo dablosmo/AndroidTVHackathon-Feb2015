a217721c745c46442e5df299b96bf3e6fc977a3e
kushaank <kushaank@usc.edu>
2015-02-07 15:14:52 -0800

    login page

--- CAHTelevision/app/src/main/java/com/androidtvhackathon_feb2015/kush2/cahtelevision/Login.java
+++ CAHTelevision/app/src/main/java/com/androidtvhackathon_feb2015/kush2/cahtelevision/Login.java
@@ -1,11 +1,36 @@
 package com.androidtvhackathon_feb2015.kush2.cahtelevision;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.*;
 /**
  * Created by kush2 on 2/7/15.
  */
-public class Login {
+public class Login extends Activity {
 
+    public void onCreate(Bundle savedInstanceState) {
 
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
+                LayoutParams.MATCH_PARENT));
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setBackgroundColor(0xff99ccff);
+
+        EditText t = new EditText(this);
+        t.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT));
+
+        linearLayout.addView(t);
+
+        // Set context view
+        setContentView(linearLayout);
+
+    }
 
 
 }
