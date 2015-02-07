5b3f11d550f4cc37dbe05860e55e1b29f7fe802c
kushaank <kushaank@usc.edu>
2015-02-07 13:36:21 -0800

    Added the CardParser to parse the csv card files

---
+++ CAHTelevision/app/src/main/java/CardParser.java
@@ -0,0 +1,41 @@
+/**
+ * Created by kush2 on 2/7/15.
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardParser {
+    InputStream inputStream;
+
+    public CardParser(InputStream inputStream){
+        this.inputStream = inputStream;
+    }
+
+    public List read(){
+        List resultList = new ArrayList();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        try {
+            String csvLine;
+            while ((csvLine = reader.readLine()) != null) {
+                String[] row = csvLine.split("\n");
+                resultList.add(row);
+            }
+        }
+        catch (IOException ex) {
+            throw new RuntimeException("Error in reading CSV file: "+ex);
+        }
+        finally {
+            try {
+                inputStream.close();
+            }
+            catch (IOException e) {
+                throw new RuntimeException("Error while closing input stream: "+e);
+            }
+        }
+        return resultList;
+    }
+}
